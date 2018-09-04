49a736c0a423629ca1e76ad23f7ab022b2e4667c
ulfvin <richard.ulfvin@addlevel.se>
2018-09-05 00:23:57 +0200

    Update schema for Product.xml

---
+++ Sample/SampleSetup/scripts/setup_SQL.ps1
@@ -0,0 +1,34 @@
+﻿Param(
+	$Instance="$env:COMPUTERNAME\SQLExpress",
+	$Database='nodeProtect',
+	$Query= $null
+)
+
+$DebugPreference = "Continue"
+$VerbosePreference = "Continue"
+
+Write-Host "Running setup_SQL.ps1"
+
+$Query = Get-Content ".\SetupTenant.sql";
+
+try{
+	$connString="Initial Catalog=$database;Data Source=$instance;;Integrated Security=SSPI;"
+	$conn=New-Object System.Data.SqlClient.SqlConnection($connString)
+	$conn.Open()
+	$cmd=$conn.CreateCommand()
+	$cmd.CommandText=$Query
+}
+catch{
+
+}
+
+
+$adapter=New-Object System.Data.SqlClient.SqlDataAdapter($cmd)
+$datatable=New-Object System.Data.DataTable
+	
+$rec=$adapter.Fill($datatable)
+
+Write-Host "Records returned=$rec"  -Fore green
+
+$conn.Close()
+$datatable
