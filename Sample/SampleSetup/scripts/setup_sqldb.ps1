0cd0d80f92fc30faec4e9f5484ac62ea36422b76
Angry Mob <angrymob@dev.addlevel.net>
2018-09-07 10:15:33 +0200

    Added SQL feature

--- Sample/SampleSetup/scripts/setup_sqldb.ps1
+++ Sample/SampleSetup/scripts/setup_sqldb.ps1
@@ -1,34 +1,55 @@
-﻿Param(
+﻿$DebugPreference = "Continue"
+$VerbosePreference = "Continue"
+$ScriptName = $MyInvocation.MyCommand.Name
+
+function Main {
+    param ()
+
+	Write-Host "Running setup_SQL.ps1"
+
+	Log -Msg ($session.CustomActionData | Format-List | Out-String)
+
+	<#
 	$Instance="$env:COMPUTERNAME\SQLExpress",
 	$Database='nodeProtect',
 	$Query= $null
-)
+	#>
 
-$DebugPreference = "Continue"
-$VerbosePreference = "Continue"
-
-Write-Host "Running setup_SQL.ps1"
-
-$Query = Get-Content ".\SetupTenant.sql";
-
-try{
+	<#
 	$connString="Initial Catalog=$database;Data Source=$instance;;Integrated Security=SSPI;"
 	$conn=New-Object System.Data.SqlClient.SqlConnection($connString)
 	$conn.Open()
 	$cmd=$conn.CreateCommand()
 	$cmd.CommandText=$Query
-}
-catch{
+	#>
 
-}
-
-
-$adapter=New-Object System.Data.SqlClient.SqlDataAdapter($cmd)
-$datatable=New-Object System.Data.DataTable
+	<#
+	$adapter=New-Object System.Data.SqlClient.SqlDataAdapter($cmd)
+	$datatable=New-Object System.Data.DataTable
 	
-$rec=$adapter.Fill($datatable)
+	$rec=$adapter.Fill($datatable)
 
-Write-Host "Records returned=$rec"  -Fore green
+	Write-Host "Records returned=$rec"  -Fore green
 
-$conn.Close()
-$datatable
+	$conn.Close()
+	$datatable
+	#>
+}
+
+function Log {
+    param ($Msg)
+    $session.Log("$ScriptName | $Msg")
+}
+function Message {
+    param ($Msg)
+    $MessageType = New-Object -TypeName Microsoft.Deployment.WindowsInstaller.InstallMessage
+    $MessageType = [Microsoft.Deployment.WindowsInstaller.InstallMessage]::ActionStart
+    $Record = New-Object -TypeName Microsoft.Deployment.WindowsInstaller.Record -ArgumentList 3
+    $Record.SetString(1, "$ScriptName")
+    $Record.SetString(2, "$Msg")
+    $session.Message($MessageType, $Record)
+}
+$StopWatch = [System.Diagnostics.Stopwatch]::StartNew()
+Main
+$StopWatch.Stop()
+Log -Msg "Completed in $(($StopWatch.Elapsed).TotalSeconds) seconds"
