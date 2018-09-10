96a0e449c1a2a63a4cd930d0c4440b49e11a2293
ulfvin <richard.ulfvin@addlevel.se>
2018-09-10 22:09:07 +0200

    Added SQL DB Setup

--- Sample/SampleSetup/scripts/setup_sqldb.ps1
+++ Sample/SampleSetup/scripts/setup_sqldb.ps1
@@ -3,37 +3,85 @@ $VerbosePreference = "Continue"
 $ScriptName = $MyInvocation.MyCommand.Name
 
 function Main {
-    param ()
+    param (
+        [string]$TenantId   = New-Guid #TODO: Get from IDP DB or setup input?,
+        [string]$DBServer   = $session.CustomActionData["SQLSERVER"],
+        [string]$DBInstance = $session.CustomActionData["SQLINSTANCE"]
+    )
 
 	Write-Host "Running setup_SQL.ps1"
 
-	Log -Msg ($session.CustomActionData | Format-List | Out-String)
+	[string]$newDB = $("$TenantId" + "-nodeProtect")
 
-	<#
-	$Instance="$env:COMPUTERNAME\SQLExpress",
-	$Database='nodeProtect',
-	$Query= $null
-	#>
+	try{
 
-	<#
-	$connString="Initial Catalog=$database;Data Source=$instance;;Integrated Security=SSPI;"
-	$conn=New-Object System.Data.SqlClient.SqlConnection($connString)
-	$conn.Open()
-	$cmd=$conn.CreateCommand()
-	$cmd.CommandText=$Query
-	#>
+		# Open ADO.NET Connection with Windows authentification:
+		$con = New-Object Data.SqlClient.SqlConnection;
+		$con.ConnectionString = "Data Source=$DBServer\$DBInstance;Initial Catalog=master;Integrated Security=True;";
+		$con.Open();
 
-	<#
-	$adapter=New-Object System.Data.SqlClient.SqlDataAdapter($cmd)
-	$datatable=New-Object System.Data.DataTable
-	
-	$rec=$adapter.Fill($datatable)
+		# Check the database:
+		$query = "SELECT name
+				  FROM sys.databases
+				  WHERE name = '$newDB';";
 
-	Write-Host "Records returned=$rec"  -Fore green
+		$cmd = New-Object Data.SqlClient.SqlCommand $query, $con;
+		$rd = $cmd.ExecuteReader();
+		if ($rd.Read())
+		{	
+			Write-Host "Database $newDB already exists";
+			$rd.Close();
+			$rd.Dispose();
+		}
+		else
+		{
+			$rd.Close();
+			$rd.Dispose();
 
-	$conn.Close()
-	$datatable
-	#>
+			# Create the database.
+			$query = "CREATE DATABASE [$newDB];"
+			$cmd = New-Object Data.SqlClient.SqlCommand $query, $con;
+			$cmd.ExecuteNonQuery();		
+			Write-Host "Database $newDB is created!";
+			$cmd.Dispose();
+
+			$query = "USE [$newDB];"
+			$cmd = New-Object Data.SqlClient.SqlCommand $query, $con;
+			$cmd.ExecuteNonQuery();
+			$cmd.Dispose();
+
+			# Create a new SQL Login and set name and password.
+			$Loginname = 'npapp'
+			$Password  = ([char[]]([char]65..[char]90) + [char[]]([char]97..[char]122) + ([char[]]([char]48..[char]57)) + 0..1 | sort {Get-Random})[0..15] -join ''
+			$query = "IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = N'$Loginname')
+						CREATE LOGIN [$Loginname] WITH PASSWORD=N'$Password', DEFAULT_DATABASE = [$newDB]"
+
+			$cmd = New-Object Data.SqlClient.SqlCommand $query, $con;
+			$cmd.ExecuteNonQuery();		
+			Write-Host "Created user: $Loginname!";
+			$cmd.Dispose();
+
+			$query = "CREATE USER [$Loginname] FOR LOGIN [$Loginname];";
+			$cmd = New-Object Data.SqlClient.SqlCommand $query, $con;
+			$cmd.ExecuteNonQuery();
+			$cmd.Dispose();
+
+			# TODO: Could just grand Read/Write or create a new role?
+			$query = "EXEC sp_addrolemember @rolename=N'db_owner', @membername=N'$Loginname';"
+			$cmd = New-Object Data.SqlClient.SqlCommand $query, $con;
+			$cmd.ExecuteNonQuery();
+			$cmd.Dispose(); 
+
+			Write-Host "Granted rights to user: $Loginname!";
+		}                
+	}
+	catch{
+
+	}
+	finally{
+		$con.Close();
+		$con.Dispose();
+	}
 }
 
 function Log {
